4191a616e4dfecd5516a94861492dab213c4063a
sahilborse18 <sahilborse.909098@gmail.com>
2024-09-23 19:54:32 +0530

    port for any IP

--- server/index.js
+++ server/index.js
@@ -47,6 +47,6 @@ io.on("connection", (socket) => {
 
 
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
